fc020a5c31c8d09b166fd3482a7e04eaaff6e026
Santiago Villa <santiagovillardz@gmail.com>
2026-05-12 18:48:01 -0600

    prueba 5 deploy

--- infra/environments/prod/main.tf
+++ infra/environments/prod/main.tf
@@ -1,6 +1,12 @@
 terraform {
   required_version = ">= 1.6.0"
 
+  backend "s3" {
+    bucket = "serp-terraform-state"
+    key    = "serp/prod/terraform.tfstate"
+    region = "us-east-1"
+  }
+  
   required_providers {
     aws = {
       source  = "hashicorp/aws"
